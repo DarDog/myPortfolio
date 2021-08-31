ac8c7004bd7b0a8751d81a9948759fcb92ff4781
DarDog <samediacc666@gmial.com>
2021-08-31 21:44:32 +1100

    feat: make scroll by click at link

--- pages/script.js
+++ pages/script.js
@@ -1,14 +1,18 @@
-const sections = Array.from(document.querySelectorAll('section'));
+const sections = Array.from(document.querySelectorAll('section')),
+    navBar = document.querySelector('.menu__list');
+
+const getLinkId = (link) => {
+  return link.getAttribute('href')
+}
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     const links = Array.from(document.querySelectorAll('.menu__link'));
-    console.log(links)
 
     if (entry.isIntersecting) {
       links.forEach(link => {
         link.classList.toggle('menu__link_active',
-            link.getAttribute('href').replace('#', '') === entry.target.id)
+            getLinkId(link).replace('#', '') === entry.target.id)
       })
     }
   })
@@ -18,4 +22,13 @@ const observer = new IntersectionObserver((entries) => {
 
 sections.forEach(section => observer.observe(section))
 
+navBar.addEventListener('click', (evt) => {
+  evt.preventDefault();
 
+  if (evt.target.classList.contains('menu__link')) {
+    window.scrollTo({
+      top: document.querySelector(getLinkId(evt.target)).offsetTop,
+      behavior: 'smooth'
+    })
+  }
+})
