01bebb6d5452e78338acfeb366a3c58fc622ee13
DarDog <samediacc666@gmial.com>
2021-12-09 11:43:56 +1100

    Feat: add about me component

---
+++ src/Main/AboutMe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section className="about main__about section" id="section-1">
+      <h2 className="title about__title">About me</h2>
+      <p className="paragraph about__paragraph">Hi, i'm Vlad - Front-end developer from russia.</p>
+      <p className="paragraph about__paragraph">I'm studying at courses "Web-developer" in "Яндекс практикум".</p>
+      <p className="paragraph about__paragraph">
+        I also have a hobby. I am fond of board and video games and I am engaged in photography at a
+        semi-professional level.</p>
+      <p className="paragraph about__paragraph">
+        In addition to front-end development, I am also interested in the back-end, but not very passionate about
+        it.</p>
+      <p className="paragraph about__paragraph">
+        If I'm interested in your company, you can contact in any convenient way. My contacts are listed at the
+        bottom of this page.</p>
+      <p className="paragraph about__paragraph">Ready to implement excellent projects with wonderful people.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import Home from "./Home";
+import AboutMe from "./AboutMe";
 
 const Main = () => {
   return (
     <main className="main page__main">
       <Home />
-      <section className="about main__about section" id="section-1">
-        <h2 className="title about__title">About me</h2>
-        <p className="paragraph about__paragraph">Hi, i'm Vlad - Front-end developer from russia.</p>
-        <p className="paragraph about__paragraph">I'm studying at courses "Web-developer" in "Яндекс практикум".</p>
-        <p className="paragraph about__paragraph">
-          I also have a hobby. I am fond of board and video games and I am engaged in photography at a
-          semi-professional level.</p>
-        <p className="paragraph about__paragraph">
-          In addition to front-end development, I am also interested in the back-end, but not very passionate about
-          it.</p>
-        <p className="paragraph about__paragraph">
-          If I'm interested in your company, you can contact in any convenient way. My contacts are listed at the
-          bottom of this page.</p>
-        <p className="paragraph about__paragraph">Ready to implement excellent projects with wonderful people.</p>
-      </section>
+      <AboutMe />
       <section className="skills main__skills section" id="section-2">
         <h2 className="title skills__title">Skills</h2>
         <p className="paragraph skills__paragraph">I work with languages and programmes ​​like</p>
