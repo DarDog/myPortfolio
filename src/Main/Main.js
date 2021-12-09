8a3babc7d0274764f278313a09493c9818794da9
DarDog <samediacc666@gmial.com>
2021-12-09 11:55:21 +1100

    Feat: add portfolio component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from "./Home";
 import AboutMe from "./AboutMe";
 import Skills from "./Skills";
+import Portfolio from "./Portfolio";
 
 const Main = () => {
   return (
@@ -9,30 +10,7 @@ const Main = () => {
       <Home />
       <AboutMe />
       <Skills />
-      <section className="portfolio main__portfolio section" id="section-3">
-        <h2 className="title portfolio__title">Portfolio</h2>
-        <ul className="list portfolio__list">
-          <li className="portfolio__item">
-            <img src="images/mesto-preview.png" alt="preview of mesto" className="portfolio__preview"/>
-            <a href="https://dardog.github.io/mesto/" className="portfolio__link" target="_blank">Landing site for
-              travel
-              photographers - Concept</a>
-          </li>
-          <li className="portfolio__item">
-            <img src="images/russian-travel-preview.png" alt="preview of russian travel"
-                 className="portfolio__preview"/>
-            <a href="https://dardog.github.io/russian-travel/" className="portfolio__link" target="_blank">Business
-              card
-              website about travel in Russia - Concept</a>
-          </li>
-          <li className="portfolio__item">
-            <img src="images/how-to-learn-preview.png" alt="preview of how to learn " className="portfolio__preview"/>
-            <a href="https://dardog.github.io/how-to-learn/" className="portfolio__link" target="_blank">Business
-              card website
-              about training</a>
-          </li>
-        </ul>
-      </section>
+      <Portfolio />
       <section className="contacts main__contacts section" id="section-4">
         <h2 className="title contacts__title">Contacts</h2>
         <p className="paragraph contacts__paragraph">Want to know more or just chat? You are welcome!</p>
